828a3f2a8acd193f4a1bda9d77d215ebd499aae6
Tello- <lollisjosh@gmail.com>
2021-03-11 08:56:32 -0800

    morning commit just in case

--- Pig.py
+++ Pig.py
@@ -21,9 +21,6 @@ class Dice:
     def roll(self):
         return random.randint(1,self.sides)
 
-class TurnData:
-    def __init__(self):
-        print("Turn Data created")
 
 class Turn:
     def __init__(self, playerNumber):
@@ -31,19 +28,53 @@ class Turn:
         self.playerNumber = playerNumber                        # Who's turn
         self.totalTurnRolls = 0                                 # How many rolls this turn
         self.rollSum = 0                                        # Turn point sum
-        self.currentRollVal = 0                                 # Most recent roll
+        self.prevRoll = 0                                       # Most recent roll
         self.firstRoll = True                                   # Flags first roll
         self.die = Dice()                                       # Die used for turn
-
+        self.rolledOne = False                                  # Flag when 1 is rolled
 
     def turnPrompt(self):
         print("Player %d, it is your turn!" %self.playerNumber)
+
+    def rollResult(self, result):
+        print("You rolled %d" %result)
         
+    def rollAgainPrompt(self) -> int:
+        choice = int(0)
+        while(int(choice) < 1) or (int(choice) > 2):
+            choice = input("Would you like to (1)Roll Again? or (2)Bank your points? >> ")
+        return choice
+
+    def takeTurn(self) -> int:
+        while True:
+            self.turnPrompt()
+            self.prevRoll = self.die.roll()
+            self.rollResult(self.prevRoll)
+
+            if(self.prevRoll == 1):
+                print("No Points For You!")
+                return 0
+            else:
+                print("You have accumulated some points")
+                choice = self.rollAgainPrompt()
+                if(choice == 1):
+                    print("You want to roll again")
+
+                elif(choice == 2):
+                    print("You would like to bank your points.")
+
+
+
+
+
+
+
+        return int() # TODO set return variable
 
     def recordRoll(self, val:int):
         self.totalTurnRolls += 1
-        self.currentRollVal = val
-        self.rollSum += self.currentRollVal
+        self.prevRoll = val
+        self.rollSum += self.prevRoll
 
 class Game:
     def __init__(self, humanPlayers:int = 1, botPlayers:int = 1, scoreGoal:int = 100):
@@ -64,10 +95,20 @@ class Game:
 
     def run(self):
         while(True):
-            print("game running")
+            for ply in self.players:
+                print(ply.number)
+                thisTurn = Turn(ply.number)
+                print("Player %d scored %d " %(ply.number, thisTurn.takeTurn()))
+            print("Game finished, Goodbye")
+            exit(0)
+                
             
    
 
    #---------------------------------------------------------------------#
                         # DEBUG AREA #
    #---------------------------------------------------------------------#
+
+
+mygame = Game(2,4,100)
+mygame.run()
